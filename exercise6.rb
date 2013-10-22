7a1a7d763b446474e6edb0823a6600e53580cc24
apoon- <anson.os.poon@gmail.com>
2013-10-22 11:56:00 -0400

    bananas, need to do method question 2

--- exercise6.rb
+++ exercise6.rb
@@ -1,11 +1,27 @@
+#Question 1 
+
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+grocery_list.each do |x|
+	puts "* #{x}"
+end
+
+#Question 2
 
 grocery_list << "rice"
 
-length = grocery_list.length
+puts grocery_list
 
-counter = (0..length-1)
+#Question 3
 
-counter.each do |x|
-	puts "* #{grocery_list[x]}"
+puts "You have #{grocery_list.length} items to pick up at the Grocery Store"
+
+#Question 4
+
+missed = grocery_list.include?("bananas")
+
+if missed == false
+	puts "You need to pick up bananas."
+elsif missed == true
+	puts "You don't need to pick up bananas today."
 end
